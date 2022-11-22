01d501582be934b688382cbf3daa55e20a0d0806
PechMV <Pechm607@gmail.com>
2022-11-22 17:51:13 -0600

    updated index.js

--- index.js
+++ index.js
@@ -63,3 +63,121 @@ function prompt() {
         }
     });
 }
+
+function viewAllEmployees() {
+    const query = 'SELECT employee.id, employee.first_name, employee.last_name';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log('VIEW ALL EMPLOYEES');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+}
+
+async function addEmployee() {
+    const addname = await inquirer.prompt(askName());
+    connection.query('SELECT role.id, role.title FROM role ORDER BY role.id;', async (err, res) => {
+        if (err) throw err;
+        const { role } = await inquirer.prompt([
+            {
+                name: 'role',
+                type: 'list',
+                choices: () => res.map(res => res.title),
+                message: 'What is the employee role?: '
+            }
+        ]);
+        let roleID;
+        for (const row of res) {
+            if (row.title === role) {
+                roleID = row.id;
+                continue;
+            }
+        }
+        connection.query('SELECT * FROM employee', async (err, res) => {
+            if (err) throw err;
+            let choices = res.map(res => '${res.first_name} ${res.last_name}');
+            choices.push('none');
+        });
+        console.log('Employee has been added. Please view all employees to verify...');
+        connection.query(
+            'INSERT INTO employee SET ?',
+            {
+                first_name: addname.first,
+                last_name: addname.last,
+                role_id: roleID,
+            },
+            (err, res) => {
+                if (err) throw err;
+                prompt();
+            }
+        );
+    });
+};
+
+function viewAllRoles() {
+    const query = 'SELECT role.title';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.log('VIEW EMPLOYEE BY ROLE');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
+}
+
+function askId() {
+    return ([
+        {
+            name: 'name',
+            type: 'input',
+            message: 'What is the employee ID?: '
+        }
+    ]);
+}
+
+async function updateRole() {
+    const employeeId = await inquirer.prompt(askId());
+    connection.query('SELECT role.id, role.title FROM role ORDER BY role.id', async (err, res) => {
+        if (err) throw err;
+        const { role } = await inquirer.prompt([
+            {
+                name: 'role',
+                type: 'list',
+                choices: () => res.map(res.title),
+                message: 'What is the new employee role?: '
+            }
+        ]);
+        let roldId;
+        for (const row of res) {
+            if (row.title === role) {
+                roleId = row.id;
+                continue;
+            }
+        }
+        connection.query(`UPDATE employee
+        SET role_id = ${roleId}
+        WHERE employeeId.id = ${employeeId.name}`, async (err, res) => {
+            if (err) throw err;
+            console.log('Role has been updated..')
+            prompt();
+        });
+    });
+}
+
+function askName() {
+    return ([
+        {
+            name: 'first',
+            type: 'input',
+            message: 'Enter the first name'
+        },
+        {
+            name: 'last',
+            type: 'input',
+            message: 'Enter the last name'
+        }
+    ]);
+}
